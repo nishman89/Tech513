2767d6f415ca51483acf51b6ee825d214010cd40
unknown <nish_mandal@hotmail.com>
2025-11-13 11:04:33 +0000

    Bank Account App updated

--- GitHubActions/src/test/java/com/sparta/clf/SavingsAccountTest.java
+++ GitHubActions/src/test/java/com/sparta/clf/SavingsAccountTest.java
@@ -3,7 +3,7 @@ package com.sparta.clf;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SavingsAccountTest {
@@ -12,6 +12,7 @@ public class SavingsAccountTest {
         SavingsAccount myAccount = new SavingsAccount(1000, 5);
         myAccount.addInterest();
         assertEquals(1050, myAccount.getBalance(), 0.001);
+        assertTrue(false);
     }
 }
 
